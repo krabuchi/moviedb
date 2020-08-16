fab156fc6c483c07a4ce77549825c28c13f6f49e
krabuchi <shashu95@yahoo.com>
2020-08-16 14:22:16 +0530

    Updates

---
+++ precache-manifest.aac01db8eb220c417a58acaf486100b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d724919e33609e596150f0935302348",
+    "url": "/moviedb/index.html"
+  },
+  {
+    "revision": "48e8837aa2535ec06924",
+    "url": "/moviedb/static/css/main.287fe228.chunk.css"
+  },
+  {
+    "revision": "960a25a0e6b8afb8c31e",
+    "url": "/moviedb/static/js/2.01dc4290.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/moviedb/static/js/2.01dc4290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48e8837aa2535ec06924",
+    "url": "/moviedb/static/js/main.50154b70.chunk.js"
+  },
+  {
+    "revision": "bea40d18fd6137245ed0",
+    "url": "/moviedb/static/js/runtime-main.e1bfa99b.js"
+  },
+  {
+    "revision": "09df7b2225f5e4508ee5e0cbb74846d7",
+    "url": "/moviedb/static/media/imdb.09df7b22.png"
+  }
+]);
